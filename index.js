9ec1160bd823d645f614dc5d819a423b537a1a74
Lsong <song940@gmail.com>
2024-06-27 13:56:35 +0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-import { ready } from 'https://lsong.org/scripts/dom.js';
-import { query } from 'https://lsong.org/scripts/query.js';
+import { ready, querySelector as $ } from 'https://lsong.org/scripts/dom.js';
 import { parse } from 'https://lsong.org/scripts/marked.js';
-import { notify } from 'https://lsong.org/scripts/notification.js';
-import { registerServiceWorker } from 'https://lsong.org/scripts/sw.js';
 
-await ai.canCreateTextSession();
-const session = await ai.createTextSession();
-const prompt = "hi";
-const stream = session.promptStreaming(prompt);
-for await (const chunk of stream) {
-  responseElement.textContent = chunk;
-}
+ready(async () => {
+  const available = await ai.canCreateTextSession();
+  if (!available) {
+    return;
+  }
+  const session = await ai.createTextSession();
+  $('#form').addEventListener('submit', async e => {
+    e.preventDefault();
+    const input = $('textarea', e.target);
+    const prompt = input.value;
+    const stream = session.promptStreaming(prompt);
+    for await (const chunk of stream) {
+      output.innerHTML = parse(chunk);
+    }
+  });
+});
